db2cf6837353a4ed121130db3105da875e0d4d6c
Eugene Sharygin <eush77@gmail.com>
2015-02-12 23:42:37 +0300

    Remove undocumented features

--- tmpdir.fish
+++ tmpdir.fish
@@ -2,16 +2,11 @@ function tmpdir
   if not set --global --query TMPDIR_TAB
     set --global --export TMPDIR_TAB (mkdir -p /tmp/tmpdir; and mktemp -d /tmp/tmpdir/XXXXXX)
   end
-  if test -t 1
-    cd $TMPDIR_TAB
-  else
-    echo $TMPDIR_TAB
-  end
+  cd $TMPDIR_TAB
 end
 
 function _tmpdir_on_exit --on-process-exit %self
-  if set --global --query TMPDIR_TAB; and test -d $TMPDIR_TAB
+  if set --global --query TMPDIR_TAB
     rm -rf $TMPDIR_TAB
-    rmdir /tmp/tmpdir >/dev/null ^&1; or true
   end
 end
