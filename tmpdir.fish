f53f03b24a7618751173f6b258e44e72d3978367
Eugene Sharygin <eush77@gmail.com>
2015-02-12 21:29:22 +0300

    Translate the original tmpdir-tab to Fish

---
+++ tmpdir.fish
@@ -0,0 +1,19 @@
+#!/usr/bin/env fish
+
+function tmpdir
+  if not set --global --query TMPDIR_TAB
+    set --global --export TMPDIR_TAB (mkdir -p /tmp/tmpdir; and mktemp -d /tmp/tmpdir/XXXXXX)
+  end
+  if test -t 1
+    cd $TMPDIR_TAB
+  else
+    echo $TMPDIR_TAB
+  end
+end
+
+function _tmpdir_on_exit --on-process-exit %self
+  if set --global --query TMPDIR_TAB; and test -d $TMPDIR_TAB
+    rm -rf $TMPDIR_TAB
+    rmdir /tmp/tmpdir >/dev/null ^&1; or true
+  end
+end
