f025b51af0c6a7a7fa605aec13e1579e686c12cc
Eugene Sharygin <eush77@gmail.com>
2015-02-13 13:00:30 +0300

    Print usage info

--- tmpdir.fish
+++ tmpdir.fish
@@ -1,32 +1,62 @@
 function tmpdir --description "cd into a fresh, one-time temporary directory"
-  switch "$argv[1]"
-    case -s --spawn
-      if set --global --query TMPDIR_TAB
-        set --local old_tmpdir_tab $TMPDIR_TAB
-        set --erase TMPDIR_TAB
-      end
+  function _tmpdir_clean
+    functions -e _tmpdir_clean
+    functions -e _tmpdir_usage
+    functions -e _tmpdir
+    functions -e _tmpdir_spawn
+  end
 
-      tmpdir
-      fish
-      cd -
-      rm -rf -- $TMPDIR_TAB
+  function _tmpdir_usage
+    printf "Usage:  tmpdir\n"
+    printf "        tmpdir [-s | --spawn]\n"
+  end
+
+  function _tmpdir
+    if set --global --query TMPDIR_TAB
+      cd $TMPDIR_TAB
+      rm -rf -- {.*,*}
+    else
+      mkdir -p /tmp/tmpdir
+      set --global TMPDIR_TAB (mktemp -d /tmp/tmpdir/XXXXXX)
+      cd $TMPDIR_TAB
+    end
+  end
+
+  function _tmpdir_spawn
+    if set --global --query TMPDIR_TAB
+      set --local old_tmpdir_tab $TMPDIR_TAB
       set --erase TMPDIR_TAB
+    end
 
-      if set --local --query old_tmpdir_tab
-        set --global TMPDIR_TAB $old_tmpdir_tab
-      end
+    _tmpdir
+    fish
+    cd -
+    rm -rf -- $TMPDIR_TAB
+    set --erase TMPDIR_TAB
 
-      return
+    if set --local --query old_tmpdir_tab
+      set --global TMPDIR_TAB $old_tmpdir_tab
+    end
   end
 
-  if set --global --query TMPDIR_TAB
-    cd $TMPDIR_TAB
-    rm -rf -- {.*,*}
-  else
-    mkdir -p /tmp/tmpdir
-    set --global TMPDIR_TAB (mktemp -d /tmp/tmpdir/XXXXXX)
-    cd $TMPDIR_TAB
+  if test (count $argv) -eq 0
+    _tmpdir
+    _tmpdir_clean
+    return
   end
+
+  if test (count $argv) -eq 1
+    switch "$argv[1]"
+      case -s --spawn
+        _tmpdir_spawn
+        _tmpdir_clean
+        return
+    end
+  end
+
+  _tmpdir_usage
+  _tmpdir_clean
+  return 1
 end
 
 
