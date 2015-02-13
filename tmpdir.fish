b32bf4603b6a6cf2c1d66eeca9c2c4b79a7f2d0b
Eugene Sharygin <eush77@gmail.com>
2015-02-13 05:50:45 +0300

    Add spawning option

--- tmpdir.fish
+++ tmpdir.fish
@@ -1,7 +1,27 @@
 function tmpdir --description "cd into a fresh, one-time temporary directory"
+  switch "$argv[1]"
+    case -s --spawn
+      if set --global --query TMPDIR_TAB
+        set --local old_tmpdir_tab $TMPDIR_TAB
+        set --erase TMPDIR_TAB
+      end
+
+      tmpdir
+      fish
+      cd -
+      rm -rf -- $TMPDIR_TAB
+      set --erase TMPDIR_TAB
+
+      if set --local --query old_tmpdir_tab
+        set --global TMPDIR_TAB $old_tmpdir_tab
+      end
+
+      return
+  end
+
   if set --global --query TMPDIR_TAB
     cd $TMPDIR_TAB
-    rm -- {.*,*}
+    rm -rf -- {.*,*}
   else
     mkdir -p /tmp/tmpdir
     set --global TMPDIR_TAB (mktemp -d /tmp/tmpdir/XXXXXX)
@@ -12,6 +32,6 @@ end
 
 function _tmpdir_on_exit --on-process-exit %self
   if set --global --query TMPDIR_TAB
-    rm -rf $TMPDIR_TAB
+    rm -rf -- $TMPDIR_TAB
   end
 end
